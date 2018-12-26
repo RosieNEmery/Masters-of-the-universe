2e6a6bbcc32c3a2b0f41a0c6e3a0c8edd75cea85
Rosie Emery <rosie_emery@hotmail.co.uk>
2018-12-26 17:02:18 +0000

    bullet texture animations

--- public/Bullet.js
+++ public/Bullet.js
@@ -2,26 +2,32 @@ class BulletStack{
   constructor(scene){
     this.scene = scene;
     this.bullet_stack = [];
+    this.tex_stack = [];
     this.bullet_vel = new THREE.Vector3(0.0, 0.05, 0.0);
     this.createMesh();
   }
 
   createMesh(){
-    let offsets = [];
+    let pos_offsets = [];
+    let tex_offsets = [];
+
     let num_instances = 1000;
 
-    this.buffer_geo = new THREE.PlaneBufferGeometry(0.1, 0.1);
+    this.buffer_geo = new THREE.PlaneBufferGeometry(0.07, 0.07);
     this.geo = new THREE.InstancedBufferGeometry();
     this.geo.index = this.buffer_geo.index;
     this.geo.attributes.position = this.buffer_geo.attributes.position;
     this.geo.attributes.uv = this.buffer_geo.attributes.uv;
 
     for (let i = 0; i < num_instances; i++){
-      offsets.push(0, 0, 0);
+      pos_offsets.push(0, 0, 0);
+      tex_offsets.push(0);
     }
 
-    this.offset_attribute = new THREE.InstancedBufferAttribute( new Float32Array(offsets), 3).setDynamic(true);
-    this.geo.addAttribute("offset", this.offset_attribute);
+    this.pos_offset_attribute = new THREE.InstancedBufferAttribute( new Float32Array(pos_offsets), 3).setDynamic(true);
+    this.tex_offset_attribute = new THREE.InstancedBufferAttribute( new Float32Array(tex_offsets), 1).setDynamic(true);
+    this.geo.addAttribute("pos_offset", this.pos_offset_attribute);
+    this.geo.addAttribute("tex_offset", this.tex_offset_attribute);
 
     let Bullet_vertShader = document.querySelector('#bullet_vertexshader');
     let Bullet_fragShader = document.querySelector('#bullet_fragmentshader');
@@ -30,13 +36,10 @@ class BulletStack{
     texture.wrapS = THREE.RepeatWrapping;
 
     let u_bullet_selection = 1.0;
-    setInterval(function() {
-    	u_bullet_selection += 1;
-    	if(u_bullet_selection > 16)
-    		u_bullet_selection = 1;
+    let t = this;
+    setInterval(function(){t.updateTextures()}, 50);
 
-    		material.uniforms.u_selection.value = u_bullet_selection;
-    }, 50);
+
     const bullet_uniforms = {
     		texture : {type: 't', value: texture},
     		u_selection: {type: 'f', value: u_bullet_selection},
@@ -46,7 +49,8 @@ class BulletStack{
     let material = new THREE.RawShaderMaterial( {
       uniforms: bullet_uniforms,
       vertexShader: Bullet_vertShader.textContent,
-      fragmentShader: Bullet_fragShader.textContent
+      fragmentShader: Bullet_fragShader.textContent,
+      transparent: true
     } );
     this.mesh = new THREE.Mesh(this.geo, material);
     this.scene.add(this.mesh);
@@ -55,6 +59,7 @@ class BulletStack{
 
   shoot(origin){
     this.bullet_stack.push(origin);
+    this.tex_stack.push(0);
   }
 
   update(){
@@ -63,17 +68,28 @@ class BulletStack{
       new_pos.addVectors(this.bullet_stack[i], this.bullet_vel)
       if (Math.abs(new_pos.y) > 3){
         this.bullet_stack.splice(i, 1);
+        this.tex_stack.splice(i, 1);
       }
       else{
-        this.bullet_stack[i] = new_pos;//this.bullet_stack[i] + this.bullet_vel;
+        this.bullet_stack[i] = new_pos;
       }
     }
     for( let i = 0; i < this.bullet_stack.length; i++){
       let index = i * 3;
-      this.offset_attribute.setXYZ(i, this.bullet_stack[i].x, this.bullet_stack[i].y, this.bullet_stack[i].z);
+      this.pos_offset_attribute.setXYZ(i, this.bullet_stack[i].x, this.bullet_stack[i].y, this.bullet_stack[i].z);
     }
     this.geo.maxInstancedCount = this.bullet_stack.length;
-    this.offset_attribute.needsUpdate = true;
+    this.pos_offset_attribute.needsUpdate = true;
+    this.tex_offset_attribute.needsUpdate = true;
+  }
+
+  updateTextures(){
+    for(let i = 0; i < this.tex_stack.length; i++){
+      let new_tex = this.tex_stack[i];
+      new_tex = (new_tex + 1)%16;
+      this.tex_stack[i] = new_tex;
+      this.tex_offset_attribute.setX(i, new_tex);
+    }
   }
 
   print(){
